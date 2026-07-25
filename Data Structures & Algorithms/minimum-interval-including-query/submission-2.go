ca3ea9fed6a777632a90467b901d47a2b46fc908
Daniel Yang <77107403+daniel-yang1739@users.noreply.github.com>
2026-07-26 00:56:30 +0800

    Add: minimum-interval-including-query - submission-2

---
+++ Data Structures & Algorithms/minimum-interval-including-query/submission-2.go
@@ -0,0 +1,22 @@
+func minInterval(intervals [][]int, queries []int) []int {
+	sort.Slice(intervals, func(i, j int) bool {
+		if intervals[i][0] < intervals[j][0] {
+			return true
+		}
+		return intervals[i][1] < intervals[j][1]
+	})
+	res := make([]int, len(queries))
+	for i, q := range queries {
+		res[i] = math.MaxInt32
+		for _, interval := range intervals {
+			L, R := interval[0], interval[1]
+			if L <= q && q <= R {
+				res[i] = min(res[i], R - L + 1)
+			}
+		}
+		if res[i] == math.MaxInt32 {
+			res[i] = -1
+		}
+	}
+	return res
+}
