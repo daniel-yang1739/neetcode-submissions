8bd0c4effb9f5c2468b09ed6a158621d89694781
Daniel Yang <77107403+daniel-yang1739@users.noreply.github.com>
2026-08-01 21:50:09 +0800

    Add: merge-triplets-to-form-target - submission-1

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-1.go
@@ -0,0 +1,14 @@
+func mergeTriplets(triplets [][]int, target []int) bool {
+	res := []int{0, 0, 0}
+	for _, t := range triplets {
+		if t[0] <= target[0] && t[1] <= target[1] && t[2] <= target[2] {
+			res[0] = max(res[0], t[0])
+			res[1] = max(res[1], t[1])
+			res[2] = max(res[2], t[2])
+			if res[0] == target[0] && res[1] == target[1] && res[2] == target[2] {
+				return true
+			}
+		}
+	}
+	return false
+}
