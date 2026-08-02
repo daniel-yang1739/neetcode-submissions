4b8ed54e275a730c8952b8e29fdc1633973ad3ce
Daniel Yang <77107403+daniel-yang1739@users.noreply.github.com>
2026-08-03 01:22:22 +0800

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.go
@@ -0,0 +1,14 @@
+func majorityElement(nums []int) int {
+    majority, mCount := 0, 0
+    for _, num := range nums {
+        if mCount == 0 {
+            majority = num
+        }
+        if num != majority {
+            mCount--
+        } else {
+            mCount++
+        }
+    }
+    return majority
+}
